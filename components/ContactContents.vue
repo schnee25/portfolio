76da2ba87b17a422d2d94971ecabf9b3fdd14de0
schnee25 <schnee.2114@gmail.com>
2021-05-10 02:42:33 +0900

    contactページ作成

---
+++ components/ContactContents.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="contactcontents">
+       <div class="contacttitle"><h1>- contact -</h1></div>
+       
+       <div class="account">
+            <div><img src="~/assets/GitHub-Logo.jpg" class="contactIconimg"></div>
+            <div class="contactmedium"><p>Github:&ensp;</p> <a href="https://github.com/schnee25" class="contactlink">schnee25</a></div>
+       </div>
+       
+       <div class="account">
+            <div><img src="~/assets/mail-icon.jpeg" class="contactIconimg"></div>
+            <div class="contactmedium"><p>Email:&ensp;</p> <p>schnee.2525yukiあっとgmail.com　/ 　あっと→@</p></div>
+       </div>
+
+       <div class="account">
+            <div><img src="~/assets/facebook-Logo.png" class="contactIconimg"></div>
+            <div class="contactmedium"><p>facebook:&ensp;</p> <a href="https://www.facebook.com/yuki.O2525">Yukino Oyamda</a></div>
+       </div>
+       
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.contactcontents {
+   
+}
+
+.contacttitle {
+    margin-top: 2%;
+    text-align: center;
+}
+.account {
+    display: flex;
+    justify-content: center;
+}
+.contactIconimg {
+    max-width: 20px;
+}
+.contactmedium{
+    padding-left: 0.5%;
+    display: flex;
+}
+
+</style>
