04d5efda8f1e62dc1229aa9680ee7e965962cc21
schnee25 <schnee.2114@gmail.com>
2021-05-10 02:43:17 +0900

    skillページに改行と / を追加

--- components/SkillContents.vue
+++ components/SkillContents.vue
@@ -1,58 +1,66 @@
 <template>
   
         <div class="skillcontents">
-            <div class="skilltitle"><h1>skill</h1></div>
+            <div class="skilltitle"><h1>- skill -</h1></div>
           <div class="exskill">
             <div class="logo">
               <img src="~/assets/HTML-Logo.png" class="skilliconimg"/>
             </div>
-              <p>HTML</p>
+              <p>　HTML　/ </p>
           </div>
 
           <div class="exskill">
             <div class="logo">
               <img src="~/assets/CSS-Logo.png"  class="skilliconimg"  />
             </div>
-              <p>CSS</p>
+              <p>　CSS　/ </p>
           </div>
 
           <div class="exskill">
             <div class="logo">
               <img src="~/assets/js-Logo.png" class="skilliconimg"/>
             </div>
-              <p>JavaScript</p>
+              <p>　JavaScript　/ </p>
           </div>
 <br>
           <div class="exskill">
             <div class="logo">
               <img src="~/assets/Microsoft-Word-Logo.png" class="skilliconimg"/>
             </div> 
-            <p class="skills">Word</p>
+            <p class="skills">　Word　/ </p>
           </div>
 
           <div class="exskill">
             <div class="logo">
               <img src="~/assets/Microsoft-Excel-Logo.png" class="skilliconimg"/>
             </div>
-            <p class="skills">Excel</p>
+            <p class="skills">　Excel　/ </p>
           </div>
 
           <div class="exskill">
             <div class="logo">
                 <img src="~/assets/Microsoft-Powerpoint-Logo.png" class="skilliconimg"/>
-            </div><p lass="skills">PowerPoint</p>
+            </div><p lass="skills">　PowerPoint　/ </p>
           </div>
+
+<br>
 
          <div class="exskill">
             <div class="logo">
                 <img src="~/assets/Git-Logo.png" class="skilliconimg"/>
-            </div><p lass="skills">git</p>
+            </div><p lass="skills">　git　/ </p>
           </div>
 
           <div class="exskill">
             <div class="logo">
                 <img src="~/assets/VisualStudioCode-Logo.png" class="skilliconimg"/>
-            </div><p lass="skills">Visual Studio Code</p>
+            </div><p lass="skills">　Visual Studio Code　/ </p>
+          </div>
+
+          <div class="exskill">
+            <div class="logo">
+                <img src="~/assets/Vue-Logo.png" class="skilliconimg"/>
+            </div><p lass="skills">　Vue.js</p>
           </div>
         
         </div>
@@ -78,20 +86,20 @@ export default {}
     text-align: center;
 }
 
-
 .exskill {
  display: inline-flex;
  
 }
 
 .skilliconimg {
-  max-width: 20px;
+  max-width: 21px;
+  max-height: 21px;
 }
 .logo {
-  height: 20px;
-  width: 20px;
-
+  height: 21px;
+  width: 21px;
 }
+
 
 
 </style>
