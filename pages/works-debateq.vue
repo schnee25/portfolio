c9f4da6a094c8db8ba2744ef08bf07406828b00d
schnee25 <schnee.2114@gmail.com>
2021-05-10 15:34:18 +0900

    ポートフォリオサイトとディベートのポータルのサイトについての詳細説明ページを作成

---
+++ pages/works-debateq.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="container">
+    <AppNameHeader></AppNameHeader>
+
+    <hr class="border" />
+
+
+    <div class="body">
+      <div class="main">
+       
+     </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+}
+
+import AppNameHeader from '../components/AppNameHeader.vue';
+components: {
+  AppNameHeader
+}
+
+
+
+
+
+
+</script>
+
+<style>
+
+</style>
