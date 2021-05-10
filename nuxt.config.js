95ecebdd955c6770c3ef73f3b2f126b2592e7917
schnee25 <schnee.2114@gmail.com>
2021-05-10 23:12:04 +0900

    error出たのでやり直し

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     babel: {
-      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+      presets({ isServer }, [preset, options]) {
+        options.loose = true
+      },
     },
   },
 }
