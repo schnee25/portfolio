2fa577cd89184db4ae9eed7af8123180582bf2ec
schnee25 <schnee.2114@gmail.com>
2021-05-11 17:43:38 +0900

    about meの詳しいページを作成

---
+++ pages/aboutme.vue
@@ -0,0 +1,97 @@
+<template>
+
+  <div class="container">
+    <AppNameHeader></AppNameHeader>
+
+    <hr class="border" />
+
+
+    <div class="body">
+        <div class="main">
+                <div class="title"><h1>Curriculum Vitae</h1></div>
+
+            <div class="biocontents">
+
+                <div class="excomponent">
+                    <h2 class="subtitle">Name</h2>
+                        <div class="separation"><p>:&ensp;</p></div>
+                    <div class="extxt">
+                        <p>小山田 雪乃</p> 
+                    </div>
+                </div>
+
+                <div class="excomponent">
+                    <h2 class="subtitle">Birth</h2>
+                        <div class="separation"><p>:&ensp;</p></div>
+                    <div class="extxt">
+                        <p>2000.12.26</p> 
+                    </div>
+                </div>
+
+                <div class="excomponent">
+                    <h2 class="subtitle">Academic background</h2>
+                        <div class="separation"><p>:&ensp;</p></div>
+                    <div class="extxt">
+                        <p>2019  熊本高校 卒業</p>
+                        <p>2020  立教大学 法学部法学科 入学</p>
+                        <p>2021  立教大学 法学部法学科 在籍</p>   
+                    </div>
+                </div>
+            
+            </div>
+        
+        </div>
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+
+import AppNameHeader from '../components/AppNameHeader.vue';
+components: {
+  AppNameHeader
+}
+
+</script>
+
+<style>
+.body {
+  font-family: 'Kiwi Maru', serif;
+}
+.main{
+  text-align:center;
+}
+h1 {
+  font-size: 130%;
+  font-weight: normal;
+}
+
+
+.subtitle {
+  display: flex;
+  font-size: 110%;
+  font-weight: normal;
+  justify-content: center;
+  width: 180px;
+}
+
+.separation{
+  font-weight: bold;
+  padding: 0% 2% 0% 2%;
+}
+.extxt{
+  text-align: left;
+}
+
+.excomponent{
+  display: flex;
+  margin: 2% 0% 2% 0%;
+}
+
+
+</style>
