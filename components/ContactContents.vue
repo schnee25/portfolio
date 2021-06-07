3690da96ef0ce9f3d9ee66c05b01c9723956abae
schnee25 <schnee.2114@gmail.com>
2021-06-07 16:59:11 +0900

    レスポンシブしやすいように書き換え&&PCでも中央の：で揃うように

--- components/ContactContents.vue
+++ components/ContactContents.vue
@@ -1,20 +1,38 @@
 <template>
     <div class="contactcontents">
-       <div class="contacttitle"><h1>- contact -</h1></div>
+       <div class="contacttitle">
+        <a name="contactlink"><h1>- contact -</h1></a>
+        </div>
        
        <div class="account">
-            <div><img src="~/assets/GitHub-Logo.jpg" class="contactIconimg"></div>
-            <div class="contactmedium"><p>Github:&ensp;</p> <a href="https://github.com/schnee25" class="contactlink" rel=”noopener noreferrer” target="_blank">schnee25</a></div>
+            <div class="imgtitle">
+                <div class="contactIconimg">
+                <img src="~/assets/GitHub-Logo.jpg" class="Iconimg">
+                </div>
+                <p>Github&emsp;:&ensp;</p>
+            </div>
+            <div class="contactmedium"> <a href="https://github.com/schnee25" class="contactlink" rel=”noopener noreferrer” target="_blank">schnee25</a></div>
        </div>
        
        <div class="account">
-            <div><img src="~/assets/mail-icon.jpeg" class="contactIconimg"></div>
-            <div class="contactmedium"><p>Email:&ensp;</p> <p>schnee.2525yukiあっとgmail.com  / <span>あっと→@</span></p></div>
+            <div class="imgtitle">
+                <div class="contactIconimg">
+                    <img src="~/assets/mail-icon.jpeg"  class="Iconimg">
+                </div>
+                <p>Email&emsp;:&ensp;</p>
+            </div>
+            <div class="contactmedium"><p>schnee.2525yukiあっとgmail.com  / <span>あっと→@</span></p></div>
        </div>
 
+
        <div class="account">
-            <div><img src="~/assets/facebook-Logo.png" class="contactIconimg"></div>
-            <div class="contactmedium"><p>facebook:&ensp;</p> <a href="https://www.facebook.com/yuki.O2525" rel=”noopener noreferrer” target="_blank">Yukino Oyamda</a></div>
+            <div class="imgtitle">
+                <div class="contactIconimg">
+                    <img src="~/assets/facebook-Logo.png" class="Iconimg">
+                </div>
+                <p>facebook&emsp;:&ensp;</p>
+            </div>
+            <div class="contactmedium"> <a href="https://github.com/schnee25" class="contactlink" rel=”noopener noreferrer” target="_blank">Yukino Oyamda</a></div>
        </div>
        
     </div>
@@ -27,7 +45,6 @@ export default {
 
 <style>
 
-
 .contacttitle {
     margin-top: 2%;
     text-align: center;
@@ -35,13 +52,57 @@ export default {
 .account {
     display: flex;
     justify-content: center;
+    padding: 3px 0 4px 0;
 }
-.contactIconimg {
+.imgtitle{
+    display: flex;
+    padding: 0 1% 0 1%;
+    
+}
+.Iconimg {
     max-width: 20px;
+    max-height: 20px;
+}
+.imgtitle{
+    display: flex;
+    min-width: 50vw;
+    justify-content: flex-end;
 }
 .contactmedium{
     padding-left: 0.5%;
     display: flex;
+    min-width: 50vw;
 }
 
+.contactIconimg{
+    padding-right: 2%;
+}
+
+
+/* レスポンシブ */
+@media screen and (max-width: 959px) {
+  /* 959px以下に適用されるCSS（タブレット用） */
+}
+@media screen and (max-width: 480px) {
+  /* 480px以下に適用されるCSS（スマホ用） */
+  .account {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding-left: 2%;
+}
+
+.imgtitle{
+    justify-content: center;
+}
+.Iconimg {
+    max-width: 20px;
+    max-height: 20px;
+}
+.contactmedium{
+    padding-bottom: 5%;
+    display: flex;
+    justify-content: center;
+}
+}
 </style>
