081e707caa2e3b5ebb9696f7a3d7b0ee813f9daf
schnee25 <schnee.2114@gmail.com>
2021-06-10 16:40:25 +0900

    works一覧的なページの作成

---
+++ pages/worksOverWiew.vue
@@ -0,0 +1,86 @@
+<template>
+
+  <div class="container">
+    <AppNameHeader></AppNameHeader>
+
+    <div class="body">
+        <div class="main">
+          <div class="cvtitle"><h1>Curriculum Vitae</h1></div>
+
+            <div class="works"></div>
+          
+
+        </div>
+    </div>
+
+    
+    <div class="footer">
+      <AppFooter></AppFooter>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+
+import AppNameHeader from '../components/AppNameHeader.vue';
+components: {
+  AppNameHeader
+}
+
+import AppFooter from '../components/AppFooter'
+components: {
+  AppFooter
+}
+
+</script>
+
+<style>
+.body {
+  font-family: 'Kiwi Maru', serif;
+  min-height: calc(100vh - 150px);
+}
+.border {
+  display: block;
+}
+
+/* .cvtitle{
+  width: 100vw;
+  text-align: center;
+  margin: 15px 0 10px 0;
+  font-size: 1.3em; 
+}
+h1 {
+  font-size: 130%;
+  font-weight: normal;
+}
+.subtitle {
+  display: flex;
+  font-size: 110%;
+  font-weight: normal;
+  justify-content: center;
+  width: 180px;
+  text-align: center;
+}
+.separation{
+  font-weight: bold;
+  padding: 0% 2% 0% 2%;
+}
+.extxt{
+  text-align: left;
+}
+p{
+  text-align: left;
+}
+.excomponent{
+  display: flex;
+  margin: 2% 0% 2% 0%;
+} */
+
+
+
+
+</style>
