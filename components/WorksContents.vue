c83b4543855d2ed6a8535ba1d343752f121ee872
schnee25 <schnee.2114@gmail.com>
2021-06-13 03:20:32 +0900

    名刺案へのルーターリンク追加

--- components/WorksContents.vue
+++ components/WorksContents.vue
@@ -1,61 +1,57 @@
 <template>
-      <div class="workscontents">
-        <div class="workstitle">
-          <a name="workslink"><h1>- works -</h1></a>
-        </div>
-          <div class="workscomponent">
-            
-          <div class="worksportpolio">
-            <router-link to="/works/portforio" class="nonboder">
-            <img src="~/assets/screanshot-portfolio.jpg" class="workss"/>
-            <p>ポートフォリオ</p>
-            </router-link>
-          </div>
-          
-          <div class="worksdebateq">
-            <router-link to="/works/debateq" class="nonboder">
-            <img src="~/assets/screanshot-debateq.jpg" class="workss"/>
-            <p>ディベート甲子園九州大会サイト</p>
-            </router-link>
-          </div>
-
-          <div class="worksportpolio">
-            <!-- <router-link to="/works/portforio" class="nonboder"> -->
-            <div class="ssimg"><img src="~/assets/screanshot-meisi.png" class="workss"/></div>
-            <p>名刺案</p>
-            <!-- </router-link> -->
-          </div>
-      
-        </div>    
+  <div class="workscontents">
+    <div class="workstitle">
+      <a name="workslink"><h1>- works -</h1></a>
+    </div>
+    <div class="workscomponent">
+      <div class="worksportpolio">
+        <router-link to="/works/portforio" class="nonboder">
+          <img src="~/assets/screanshot-portfolio.jpg" class="workss" />
+          <p>ポートフォリオ</p>
+        </router-link>
       </div>
+
+      <div class="worksdebateq">
+        <router-link to="/works/debateq" class="nonboder">
+          <img src="~/assets/screanshot-debateq.jpg" class="workss" />
+          <p>ディベート甲子園九州大会サイト</p>
+        </router-link>
+      </div>
+
+      <div class="worksportpolio">
+        <router-link to="/works/meisi" class="nonboder">
+          <div class="ssimg"><img src="~/assets/screanshot-meisi.png" class="workss" /></div>
+          <p>名刺案</p>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style>
 .workscontents {
-    padding-top: 3%;
-    margin: 0 auto;
-    margin-top: 2%;
-    text-align: center;
+  padding-top: 3%;
+  margin: 0 auto;
+  margin-top: 2%;
+  text-align: center;
 }
-.worksportpolio,.worksdebateq{
+.worksportpolio,
+.worksdebateq {
   display: inline-block;
   margin: 3% 3% 1% 3%;
 }
 
-.workss{
-   max-width: 250px;
-   max-height: 250px;
-   margin-bottom: 2%;
+.workss {
+  max-width: 250px;
+  max-height: 250px;
+  margin-bottom: 2%;
 }
-.ssimg{
+.ssimg {
   min-height: 200px;
   min-width: 250px;
-  }
-  
-
+}
 </style>
