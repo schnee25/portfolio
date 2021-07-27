8c0174cdec747081e66933ae282e95ad62d7c6b3
schnee25 <schnee.2114@gmail.com>
2021-07-27 16:27:23 +0900

    CSSコメントアウトを元に戻した

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,8 @@ export default {
 
   plugins: ['~plugins/vue-scrollto'],
 
-  // // Global CSS: https://go.nuxtjs.dev/config-css
-  // css: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
